2e547d802ed864cfa9cbdac792d799e0b35ea3b6
woowee <wooweezoowee@gmail.com>
2014-04-19 07:46:47 +0900

    modifird sha2, missmatch? delete blank space?

--- Casks/mytracks.rb
+++ Casks/mytracks.rb
@@ -2,6 +2,6 @@ class Mytracks < Cask
   url 'http://www.mytracks4mac.info/download/myTracks2.dmg'
   homepage 'http://www.mytracks4mac.com/'
   version '2.5.3.2'
-  sha256 'bbd0c2f6489c604b4da5cd7afab5785e46fc9b963955c0961d18339f4826daf2 '
+  sha256 'bbd0c2f6489c604b4da5cd7afab5785e46fc9b963955c0961d18339f4826daf2'
   link '/Applications/myTracks.app'
 end
