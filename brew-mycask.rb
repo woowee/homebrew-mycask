cde95caca013f264bd7d76ad4254ce63125a2cb7
woowee <wooweezoowee@gmail.com>
2014-05-03 11:51:10 +0900

    modified homepage and url

--- brew-mycask.rb
+++ brew-mycask.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class BrewMycask < Formula
-  homepage 'https://github.com/sonots/homebrew-mycask/'
-  url 'https://github.com/sonots/homebrew-mycask.git'
+  homepage 'https://github.com/woowee/homebrew-mycask/'
+  url 'https://github.com/woowee/homebrew-mycask.git'
   def install
   end
 end
