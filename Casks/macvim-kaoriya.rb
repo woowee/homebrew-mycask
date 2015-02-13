08254049d9efa10a33f228eba22452fe749134ee
woowee <wooweezoowee@gmail.com>
2015-02-14 06:58:55 +0900

    modified formulas.

--- Casks/macvim-kaoriya.rb
+++ Casks/macvim-kaoriya.rb
@@ -1,8 +1,9 @@
-class MacvimKaoriya < Cask
+cask :v1 => 'macvim-kaoriya' do
   version 'latest'
-  sha256 'a47e42e32ddb3dd26e65e1ccfac532474da2a89fb516eb3a480b4f0ddabe4235'
+  sha256 '33c49dc457f24a30122223324b0fa94f230ec8e3cd1a41929fcd06183fc9c550'
 
-  url 'https://github.com/splhack/macvim/releases/download/20140805/macvim-kaoriya-20140805.dmg'
+  url 'https://github.com/splhack/macvim/releases/download/20150211/macvim-kaoriya-20150211.dmg'
+  name 'macvim-kaoriya'
   homepage 'https://code.google.com/p/macvim-kaoriya/'
   license :unknown
 
--- Casks/mytracks.rb
+++ Casks/mytracks.rb
@@ -1,8 +1,9 @@
-class Mytracks < Cask
+cask :v1 => 'mytracks' do
   version 'latest'
-  sha256 '39cc6e40ab6c239c6102256cd85658d9a4c1e574dc17a40fde92479dcc19cf5d'
+  sha256 '2e51df5efe9cd3c41a62235a71892e4afe1eaf70c6a64720cda11913c1c49bec'
 
   url 'http://www.mytracks4mac.info/download/myTracks2.dmg'
+  name 'mytracks'
   homepage 'http://www.mytracks4mac.info/c/index.php/en/'
   license :unknown
 
