f5c198e9825b02456151487cb4992bfab523f58b
woowee <wooweezoowee@gmail.com>
2017-01-14 08:39:30 +0900

    mytracks.rb:Add.;

---
+++ Casks/mytracks.rb
@@ -0,0 +1,10 @@
+cask 'mytracks' do
+  version '2.6.10'
+  sha256 'c6eed0a5dee8cccd88c19497c39748af0dcd7385785667c763246b137227cff1'
+
+  url 'http://www.mytracks4mac.info/download/myTracks2.dmg'
+  name 'myTracks'
+  homepage 'http://www.mytracks4mac.info/index.php/en/'
+
+  app 'myTracks.app'
+end
