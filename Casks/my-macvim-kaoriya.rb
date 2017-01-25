d769fd6801e450567c489185d5a4664a471a5e5e
woowee <wooweezoowee@gmail.com>
2017-01-25 19:47:43 +0900

    test again

---
+++ Casks/my-macvim-kaoriya.rb
@@ -0,0 +1,10 @@
+cask 'my-macvim-kaoriya' do
+  version ':latest'
+  sha256 '6be1450f0456cac573849e00e499f250054aa771f927d03a9f26edf7d1a95477'
+
+  url 'https://github.com/splhack/macvim-kaoriya/archive/master.zip'
+  name 'my-macvim-kaoriya'
+  homepage 'https://github.com/splhack/macvim-kaoriya'
+
+	app 'MacVim.app'
+end
